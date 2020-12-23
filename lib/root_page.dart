c31b0719f9f98d3ad0e1802170f002a34f580d4f
dobby90 <extremezerg@naver.com>
2020-12-23 14:29:12 +0900

    Screen Design

---
+++ lib/root_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:instargram_clone_with_flutter/tab_page.dart';
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TabPage();
+  }
+}
---
+++ lib/tab_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+  List _pages = [Text('Home Page'), Text('Search Page'), Text('Account Page')];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _pages[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          ),
+        ],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
