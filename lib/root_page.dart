1a641be0fa32a09e7aa86de7168d45876face4d0
dobby90 <extremezerg@naver.com>
2020-12-24 17:37:13 +0900

    Login Page UI

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:instargram_clone_with_flutter/login_page.dart';
 import 'package:instargram_clone_with_flutter/tab_page.dart';
 
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TabPage();
+    return LoginPage();
   }
 }
