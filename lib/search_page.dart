d75632b7b3602ab4583666b587a7c8f22f4f198d
dobby90 <extremezerg@naver.com>
2020-12-29 15:03:14 +0900

    detail view create

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instargram_clone_with_flutter/create_page.dart';
+import 'package:instargram_clone_with_flutter/detail_post_page.dart';
 
 class SearchPage extends StatefulWidget {
   final User user;
@@ -66,9 +67,18 @@ class _SearchPageState extends State<SearchPage> {
   }
 
   Widget _buildListItem(context, document) {
-    return Image.network(
-      document['photoUrl'],
-      fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return DetailPostPage(document);
+          },
+        ));
+      },
+      child: Image.network(
+        document['photoUrl'],
+        fit: BoxFit.cover,
+      ),
     );
   }
 }
